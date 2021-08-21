39a823e1f52ece4dcb13c1fcb7f9147955f5b31d
Michael Efimov <schumixer@list.ru>
2021-08-21 20:04:48 +0300

    item sending

--- users_items_project/models.py
+++ users_items_project/models.py
@@ -1,5 +1,5 @@
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, url_for
 from users_items_project import db, login_manager
 from flask_login import UserMixin
 
@@ -8,37 +8,52 @@ from flask_login import UserMixin
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
-
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    items = db.relationship('Items', backref='author', lazy=True)
+    items = db.relationship("Items", backref="author", lazy=True)
 
     def get_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
+        s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
+        return s.dumps({"user_id": self.id}).decode("utf-8")
+    
+    @staticmethod
+    def get_link(user_login_to, item_id, expires_sec=1800):
+        s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
+        token = s.dumps({"user_login_to": user_login_to, "item_id": item_id}).decode("utf-8")
+        link = url_for("users.get", token=token, _external=True)
+        return link
 
     @staticmethod
     def get_user_with_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+        s = Serializer(current_app.config["SECRET_KEY"])
         try:
-            user_id = s.loads(token)['user_id']
+            user_id = s.loads(token)["user_id"]
         except:
             return None
         return Users.query.get(user_id)
+    
+    @staticmethod
+    def get_data_from_link(token):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return data
 
     def __repr__(self):
         return f"Users('{self.username}')"
 
+
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.name}')"
-    
-    
+
     def to_json(self):
-        return {"name": self.name}
+        return {"id": self.id, "name": self.name}
--- users_items_project/users/routes.py
+++ users_items_project/users/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, url_for, flash, redirect, request, Blueprint, jsonify, abort
-from flask_login import login_user, current_user, logout_user, login_required
+from flask import  redirect, request, Blueprint, jsonify, abort
+from sqlalchemy.orm import load_only
 from users_items_project import db
-from users_items_project.models import Users
+from users_items_project.models import Users, Items
 from users_items_project.users.utils import is_valid_login
 
 
@@ -41,4 +41,43 @@ def login():
         return jsonify(response)
     abort(401)
         
+@users.route("/send", methods=['POST'])
+def send():
+    print(request.json)
+    if not request.json:
+        abort(400)
+    user_from = Users.get_user_with_token(request.json.get("token"))
+    if user_from:
+        user_to =  Users.query.filter_by(login = request.json.get("login")).first()
+        if user_to:
+            selected_item =  Items.query.filter_by(author=user_from, id = int(request.json.get("id"))).first()
+            if selected_item:
+            
+                response = {
+                    "link": Users.get_link(user_login_to=user_to.login, item_id=selected_item.id)
+                }    
+                    
+                return jsonify(response)
+    abort(401)
 
+
+@users.route("/get/<token>", methods=['GET'])
+def get(token):
+    print(request.json)
+    if not request.json:
+        abort(400)
+    user = Users.get_user_with_token(request.json.get("token"))
+    if user:
+        data = Users.get_data_from_link(token)
+        user_from_login = data["user_login_to"]
+        item_id = data["item_id"]
+        # user_login = Items.query.get(item_id).author.login
+        if user.login == user_from_login:
+            Items.query.get(item_id).user_id = Users.query.filter_by(login = user_from_login).first().id
+            db.session.commit()
+            response = {
+                "description": "The item was moved"
+            }
+            return jsonify(response), 201
+    abort(401)
+    
